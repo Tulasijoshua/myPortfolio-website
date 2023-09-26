9a584d90af8addc1bbf1abca9935b8418bee3d98
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-09-26 16:23:25 +0000

    Feat: portfolio website built

---
+++ src/containers/portfolio/index.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { BsInfoCircleFill } from "react-icons/bs";
+import ImageOne from "../../images/image1.jpg";
+import ImageTwo from "../../images/image2.jpg";
+import ImageThree from "../../images/image3.jpg";
+import ImageFour from "../../images/image4.jpg";
+import ImageFive from "../../images/image5.jpg";
+import "./styles.scss";
+import { useState } from "react";
+
+const portfolioData = [
+  {
+    id: 2,
+    name: "Movies web application",
+    image: ImageOne,
+    link: "https://hngx-movieapp.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Gallery image app",
+    link: "https://hngimage-gallery.netlify.app/",
+    image: ImageTwo,
+  },
+  {
+    id: 2,
+    name: "Advance todo app",
+    image: ImageThree,
+    link: "https://todo-schoolinka.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Event management system",
+    image: ImageFour,
+
+    link: "https://www.dijievents.com/",
+  },
+  {
+    id: 3,
+    name: "Tropitech",
+    image: ImageFive,
+    link: "",
+  },
+];
+
+const filterData = [
+  {
+    filterId: 1,
+    label: "All",
+  },
+  {
+    filterId: 2,
+    label: "Developement",
+  },
+  {
+    filterId: 3,
+    label: "Design",
+  },
+];
+
+const Portfolio = () => {
+  const [filteredvalue, setFilteredValue] = useState(1);
+  const [hoveredValue, setHoveredValue] = useState(null);
+
+  function handleFilter(currentId) {
+    setFilteredValue(currentId);
+  }
+
+  function handleHover(index) {
+    setHoveredValue(index);
+  }
+
+  console.log("====================================");
+  console.log(hoveredValue);
+  console.log("====================================");
+
+  const filteredItems =
+    filteredvalue === 1
+      ? portfolioData
+      : portfolioData.filter((item) => item.id === filteredvalue);
+
+  console.log(filteredItems);
+
+  return (
+    <section id="portfolio" className="portfolio">
+      <PageHeaderContent
+        headerText="My Portfolio"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filterData.map((item) => (
+            <li
+              className={item.filterId === filteredvalue ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+        <div className="portfolio__content__cards">
+          {filteredItems.map((item, index) => (
+            <div
+              className="portfolio__content__cards__item"
+              key={`cardItem${item.name.trim()}`}
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <a>
+                  <img alt="dummy data" src={item.image} />
+                </a>
+              </div>
+              <div className="overlay">
+                {index === hoveredValue && (
+                  <div>
+                    <p>{item.name}</p>
+                    <a href={item.link}>
+                      <button >
+
+                        Visit</button>
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Portfolio;
